c49b9eb75b250dca016cc49cce4ab304ec72816d
WheresAlice <wheresalice@noreply.users.github.com>
2022-06-25 11:31:47 +0100

    feat: cache results from facebook for 5 minutes

--- faceprox.go
+++ faceprox.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	faceloader "github.com/geeksforsocialchange/faceloader/parser"
 	"github.com/gorilla/mux"
+	"github.com/patrickmn/go-cache"
 	"log"
 	"net/http"
 	"time"
@@ -20,7 +21,11 @@ var indexPage string
 //go:embed static/events.html
 var eventsPage string
 
+var c *cache.Cache
+
 func main() {
+	c = cache.New(5*time.Minute, 10*time.Minute)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/events/{key}.json", EventDataHandler)
@@ -54,35 +59,65 @@ func PageHandler(w http.ResponseWriter, r *http.Request) {
 
 func EventDataHandler(w http.ResponseWriter, r *http.Request) {
 	var result map[string]interface{}
+	var err error
 
 	vars := mux.Vars(r)
 	eventUrl := fmt.Sprintf("https://mbasic.facebook.com/events/%v", vars["key"])
-	result, err := faceloader.InterfaceFromMbasic(eventUrl)
-	if err != nil {
-		log.Println(err)
+	cachedResult, found := c.Get(eventUrl)
+	if found {
+		log.Println("cache hit")
+		result = cachedResult.(map[string]interface{})
+	} else {
+		log.Println("cache miss")
+		result, err = faceloader.InterfaceFromMbasic(eventUrl)
+		if err != nil {
+			log.Println(err)
+		}
+		c.Set(eventUrl, result, cache.DefaultExpiration)
 	}
 	json.NewEncoder(w).Encode(result)
 }
 
 func PageDataHandler(w http.ResponseWriter, r *http.Request) {
 	var events []interface{}
+	var mbasicLinks []string
+	var err error
 
 	vars := mux.Vars(r)
 	pageName := vars["key"]
 
-	mbasicLinks, err := faceloader.GetFacebookEventLinks(pageName)
-	mbasicLinks = faceloader.RemoveDuplicateStr(mbasicLinks)
-
-	if err != nil {
-		log.Println(err)
-	}
-	for i := range mbasicLinks {
-		log.Println(i, mbasicLinks[i])
-		e, err := faceloader.InterfaceFromMbasic(mbasicLinks[i])
+	cachedLinks, found := c.Get(pageName)
+	if found {
+		log.Println("cache hit")
+		mbasicLinks = cachedLinks.([]string)
+	} else {
+		log.Println("cache miss")
+		mbasicLinks, err = faceloader.GetFacebookEventLinks(pageName)
+		mbasicLinks = faceloader.RemoveDuplicateStr(mbasicLinks)
 		if err != nil {
 			log.Println(err)
+		} else {
+			c.Set(pageName, mbasicLinks, cache.DefaultExpiration)
 		}
-		events = append(events, e)
+	}
+	for i := range mbasicLinks {
+		link := mbasicLinks[i]
+
+		event, found := c.Get(link)
+		if found {
+			log.Println("cache hit")
+			events = append(events, event.(interface{}))
+		} else {
+			log.Println("cache miss")
+			e, err := faceloader.InterfaceFromMbasic(mbasicLinks[i])
+			if err != nil {
+				log.Println(err)
+			} else {
+				c.Set(link, e, cache.DefaultExpiration)
+			}
+			events = append(events, e)
+		}
+
 	}
 	json.NewEncoder(w).Encode(events)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/geeksforsocialchange/faceloader v0.0.0-20220617154812-244a699dab05
 	github.com/gorilla/mux v1.8.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 )
 
 require (
